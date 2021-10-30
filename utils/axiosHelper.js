8ec829779a27c2ad33e01e9f18d59d913f5b4505
quangvu30 <quangvd3011@gmail.com>
2021-10-30 15:52:18 +0700

    add post method axios

--- utils/axiosHelper.js
+++ utils/axiosHelper.js
@@ -16,4 +16,28 @@ async function getTicket(ticket_id) {
     return err;
   }
 }
+
+async function validTransaction(transactionData) {
+  let config = {
+    method: "post",
+    url: "http://54.255.155.203:3001/ticket/validTicket",
+    data: {
+      ticket_id: "",
+      blockNumber: "9553574",
+      transactionHash:
+        "0xaf0f0ed596c7848e429f67bcc145e95221f40d165327657ccfa02acd10f4974b",
+      to: "0x09453c14D12603B4dD237991e8FDf52251EA0529",
+      value: "10",
+      chainId: 4,
+      typeToken: "busd",
+    },
+  };
+
+  try {
+    let result = await axios(config);
+    return result.data;
+  } catch (err) {
+    return err;
+  }
+}
 module.exports = { getTicket };
